7a3c3db1e5e59158f57b91736bebfc372220e682
Sebastian Kinnewig <Sebastian@Kinnewig.org>
2024-04-24 14:32:45 +0200

    Add LSP: nvim-lspconfig

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,4 +1,15 @@
 local plugins = {
+  -- Offical LSP (Language Server Protocol)
+  --   necessary for autocompletion
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  -- Mason Package manager
   {
     "williamboman/mason.nvim",
     opts = {
