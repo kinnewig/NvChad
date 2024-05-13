d19d2e3a2ea596d76d302d1fce7ae1a4f02c20f9
Sebastian Kinnewig <sebastian@kinnewig.org>
2024-05-13 11:11:43 +0200

    Configure nvterm.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,6 +24,36 @@ local default_plugins = {
       require("core.utils").load_mappings "nvterm"
     end,
     config = function(_, opts)
+      opts.terminals = {
+        shell = vim.o.shell,
+        list = {},
+        type_opts = {
+          float = {
+            relative = 'editor',
+            row = 0.3,
+            col = 0.25,
+            width = 0.5,
+            height = 0.4,
+            border = "single",
+          },
+          horizontal = {
+            location = "rightbelow",
+            split_ratio = 0.3,  -- Adjust this value to change the height
+          },
+          vertical = {
+            location = "rightbelow",
+            split_ratio = 0.5,
+          },
+        },
+      }
+      opts.behavior = {
+        autoclose_on_quit = {
+          enabled = false,
+          confirm = true,
+        },
+        close_on_exit = true,
+        auto_insert = true,
+      }
       require "base46.term"
       require("nvterm").setup(opts)
     end,
